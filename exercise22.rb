07d313d3b360d2b0e8d7ef965f5a5c7276c72db0
Syeda-Nadeem <syeda.nadeem@hotmail.com>
2022-03-15 15:53:29 +0000

    exercise 23

--- exercise22.rb
+++ exercise22.rb
@@ -30,4 +30,4 @@ puts "We'd have #{beans} beans #{jars} jars and #{crates} crates."
 
 start_point = start_point/10
 puts "We can also do this way."
-puts "We'd have %s beans, %d jars, and %d crates." % secret_formula(start_point)
+puts "We'd have %s beans, %d jars, and  %i crates." % secret_formula(start_point)
